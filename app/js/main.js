9869953f4311938a4ad12748c5094cda52fcaae8
1ns1derDn <sergienko-98@mail.ua>
2020-02-15 23:19:35 +0200

    addet section features-slider/products/products-slider/statistics/advantage/opportunities/feedback

---
+++ app/js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+  $('.products-slider__inner').slick({
+    appendArrows: $('.products-slider__btn'),
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
+
+  $('.features-slider__inner').slick({
+    appendArrows: $('.features-slider__btn'),
+    arrows: true,
+    fade: true
+  });
+  
+  $('.features-slider__inner-stars').rateYo({
+    rating: 5,
+    starWidth: "15px",
+    halfStar: true
+  });
+  
+});
