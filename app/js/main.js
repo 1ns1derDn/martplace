2d5dc9a8bf5477ca91526428c985488288d932f4
1ns1derDn <sergienko-98@mail.ua>
2020-02-19 17:18:41 +0200

    added header drop menu

--- app/js/main.js
+++ app/js/main.js
@@ -17,5 +17,94 @@ $(function(){
     starWidth: "15px",
     halfStar: true
   });
+
+  $('.bell-number__link').on('click', function(){
+    $('.notifications__menu').slideToggle();
+
+    if($('.bell-number__link').hasClass('active'))
+    {
+      $('.bell-number__link').removeClass('active');
+    }else{
+    $('.bell-number__link').addClass('active');
+    }
+  });
+
+  $('.message__link').on('click', function(){
+    $('.message__menu').slideToggle();
+
+    if($('.message__link').hasClass('active'))
+    {
+      $('.message__link').removeClass('active');
+    }else{
+    $('.message__link').addClass('active');
+    }
+  });
+
+  $('.basket__link').on('click', function(){
+    $('.basket__menu').slideToggle();
+
+    if($('.basket__link').hasClass('active'))
+    {
+      $('.basket__link').removeClass('active');
+    }else{
+    $('.basket__link').addClass('active');
+    }
+  });
+
+  $('.first-drop').on('click', function(){
+    $('.first-drop__home').slideToggle();
+
+    if($('.first-drop').hasClass('active'))
+    {
+      $('.first-drop').removeClass('active');
+    }else{
+    $('.first-drop').addClass('active');
+    }
+  });
+
+
+
+  $('.second-drop').on('click', function(){
+    $('.second-drop__wordpress').slideToggle();
+
+    if($('.second-drop').hasClass('active'))
+    {
+      $('.second-drop').removeClass('active');
+    }else{
+    $('.second-drop').addClass('active');
+    }
+  });
+
+  $('.three-drop').on('click', function(){
+    $('.three-drop__products').slideToggle();
+
+    if($('.three-drop').hasClass('active'))
+    {
+      $('.three-drop').removeClass('active');
+    }else{
+    $('.three-drop').addClass('active');
+    }
+  });
   
+  $('.pages').on('click', function(){
+    $('.pages__menu').slideToggle();
+
+    if($('.pages').hasClass('active'))
+    {
+      $('.pages').removeClass('active');
+    }else{
+    $('.pages').addClass('active');
+    }
+  });
+
+  $('.header-top__user').on('click', function(){
+    $('.user__menu').slideToggle();
+
+    if($('.header-top__user').hasClass('active'))
+    {
+      $('.header-top__user').removeClass('active');
+    }else{
+    $('.header-top__user').addClass('active');
+    }
+  });
 });
