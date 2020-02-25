e01fc0c1c787b72b60ccdbd6121b321f7d895337
1ns1derDn <sergienko-98@mail.ua>
2020-02-25 17:37:18 +0200

    added tabs

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,14 @@
 $(function(){
 
+  $('.wrapper .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+      $('.wrapper').find('.tab-item').removeClass('active-tab').hide();
+      $('.wrapper .tabs').find('.tab').removeClass('active');
+      $(this).addClass('active');
+      $('#'+id).addClass('active-tab').fadeIn();
+      return false;
+    });
+
   $('input, select').styler();
 
   $('.products-slider__inner').slick({
